cbb276db0a35761ac5dddee70861e42419737e5b
bhoyarnikhil12 <140892322+bhoyarnikhil12@users.noreply.github.com>
2024-07-11 17:39:43 +0530

    Create ProductData.js

---
+++ src/Componants/ProductData.js
@@ -0,0 +1,52 @@
+const NewData = [
+  {
+    id: 1, name: "Crew Neck T-shirt", cover: "https://assets.ajio.com/medias/sys_master/root/20230602/JRvr/64793436d55b7d0c6336ff24/-473Wx593H-461571257-blue-MODEL.jpg",
+    color: 'blue', price: '600.00', description: '100% Organic Cotton	A soft, breathable jersey which is an excellent choice for the warmest day. It will mix great with any pair of jeans'
+  },
+  {
+    id: 2, name: "Denim Fit T-shirt", cover: "https://sslimages.shoppersstop.com/sys-master/images/hcf/h33/17548392398878/S21509IRNRW_DEEP_INDIGO_alt1.jpg_2000Wx3000H",
+    color: 'gray', price: '680.00', description: '100% Organic Cotton	A soft, breathable jersey which is an excellent choice for the warmest day. It will mix great with any pair of jeans'
+  },
+  {
+    id: 3, name: "Tank Top T-shirt", cover: "https://proline.in/media/catalog/product/cache/4f84cf47500489bc87b733ace13f31cb/p/a/pa133_olsd_1.jpg",
+    color: 'green', price: '720.00', description: '100% Organic Cotton	A soft, breathable jersey which is an excellent choice for the warmest day. It will mix great with any pair of jeans'
+  },
+  {
+    id: 4, name: "Leather Quarts Mens Watch", cover: "https://images-cdn.ubuy.co.in/63587a899982811cbe7375a3-lige-men-039-s-watches-top-brand.jpg",
+    color: 'catechu', price: '1140.00', description: 'A watch is a portable timepiece intended to be carried or worn by a person. It is designed to keep a consistent movement despite the motions caused by the persons activities.'
+  },
+  {
+    id: 5, name: "OLEVES Classic Mens Watch", cover: "https://m.media-amazon.com/images/I/710phncCIkL._AC_UY1000_.jpg",
+    color: 'yellow', price: '1560.00', description: 'A watch is a portable timepiece intended to be carried or worn by a person. It is designed to keep a consistent movement despite the motions caused by the persons activities.'
+  },
+  {
+    id: 6, name: "Mesh band Quartz Mens Watch", cover: "https://canary.contestimg.wish.com/api/webimage/5d3e63f2e2eab51b40dfec7b-large.jpg?cache_buster=98d96194fe5315397ae7214b71b01d3e",
+    color: 'yellow', price: '1220.00', description: 'A watch is a portable timepiece intended to be carried or worn by a person. It is designed to keep a consistent movement despite the motions caused by the persons activities.'
+  },
+  {
+    id: 7, name: "Alberto Torresi Mens Footwear", cover: "https://www.albertotorresi.com/cdn/shop/products/88709-TAN_9_600x.jpg?v=1686979885",
+    color: 'catechu', price: '820.00', description: 'Footwear refers to garments worn on the feet, which typically serve the purpose of protection against adversities of the environment such as wear from rough ground'
+  },
+  {
+    id: 8, name: "Leather Casual Mens  Footwear", cover: "https://5.imimg.com/data5/SELLER/Default/2022/9/JP/SJ/ED/38630640/casual-shoes-500x500.jpg",
+    color: 'White', price: '750.00', description: 'Footwear refers to garments worn on the feet, which typically serve the purpose of protection against adversities of the environment such as wear from rough ground'
+  },
+  {
+    id: 9, name: "Business casual Mens Footwear", cover: "https://images-cdn.ubuy.co.in/6353b7ab56c46e05641f5053-mens-shoes-fashion-classic-style-british.jpg",
+    color: 'catechu', price: '940.00', description: 'Footwear refers to garments worn on the feet, which typically serve the purpose of protection against adversities of the environment such as wear from rough ground'
+  },
+  {
+    id: 10, name: "Leather Mens Wallet", cover: "https://www.promotionalwears.com/image/cache/catalog/data/gifts-set/genuine-leather-mens-wallet-brown/brown-leather-men-wallet-inside-750x750.jpg",
+    color: 'brown', price: '400.00', description: 'A wallet carries your cash, debit and credit cards, drivers license, business cards, keys, receipts, photos of your loved ones, and much more'
+  },
+  {
+    id: 11, name: "Lather Mens Wallet with Detacheable Card", cover: "https://hideandskin.in/wp-content/uploads/2022/10/tan-tam.jpg",
+    color: 'catechu', price: '480.00', description: 'A wallet carries your cash, debit and credit cards, drivers license, business cards, keys, receipts, photos of your loved ones, and much more'
+  },
+  {
+    id: 12, name: "Branded Mens Wallet", cover: "https://img.tatacliq.com/images/i17//437Wx649H/MP000000021728453_437Wx649H_202403290523504.jpeg",
+    color: 'black', price: '560.00', description: 'A wallet carries your cash, debit and credit cards, drivers license, business cards, keys, receipts, photos of your loved ones, and much more'
+  },
+]
+
+export default NewData;
